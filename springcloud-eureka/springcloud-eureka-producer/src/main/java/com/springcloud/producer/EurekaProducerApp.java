97391211f552c1cf8b7ec70e243ea84ca074739c
yunnasheng <yunnasheng@payeasenet.com>
2024-08-08 20:25:35 +0800

    spring boot 升级为3.0.13, spring cloud 升级为2022.0.5

--- springcloud-eureka/springcloud-eureka-producer/src/main/java/com/springcloud/producer/EurekaProducerApp.java
+++ springcloud-eureka/springcloud-eureka-producer/src/main/java/com/springcloud/producer/EurekaProducerApp.java
@@ -2,11 +2,20 @@ package com.springcloud.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@EnableEurekaClient
+// 新版本已经废弃了@EnableEurekaClient, 改用@EnableDiscoveryClient注解
+@EnableDiscoveryClient
 @SpringBootApplication
+@RestController
 public class EurekaProducerApp {
+
+	@RequestMapping("/")
+	public String home(){
+		return "Welcome";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaProducerApp.class, args);
--- springcloud-eureka/springcloud-eureka-server/src/main/java/com/springcloud/eureka/EurekaServerApp.java
+++ springcloud-eureka/springcloud-eureka-server/src/main/java/com/springcloud/eureka/EurekaServerApp.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer // 启用服务发现，注册中心
+@EnableEurekaServer
 @SpringBootApplication // springboot 核心配置
 public class EurekaServerApp {
 
