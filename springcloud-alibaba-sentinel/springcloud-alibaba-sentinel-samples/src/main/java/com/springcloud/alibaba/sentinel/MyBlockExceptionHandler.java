c097569a4cca744c6051d6f1d4637de9831c1b25
yunnasheng <yunnasheng@payeasenet.com>
2024-09-03 11:54:01 +0800

    sentinel 动态错误信息

--- springcloud-alibaba-sentinel/springcloud-alibaba-sentinel-samples/src/main/java/com/springcloud/alibaba/sentinel/MyBlockExceptionHandler.java
+++ springcloud-alibaba-sentinel/springcloud-alibaba-sentinel-samples/src/main/java/com/springcloud/alibaba/sentinel/MyBlockExceptionHandler.java
@@ -2,6 +2,12 @@ package com.springcloud.alibaba.sentinel;
 
 import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.BlockExceptionHandler;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.authority.AuthorityException;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeException;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowException;
+import com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowException;
+import com.alibaba.csp.sentinel.slots.system.SystemBlockException;
+import com.alibaba.fastjson.JSON;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -27,14 +33,36 @@ public class MyBlockExceptionHandler implements BlockExceptionHandler {
     public void handle(HttpServletRequest request, HttpServletResponse response, BlockException e) throws Exception {
         URI uri = URI.create(request.getRequestURI());
 
-        log.info(".....................................MyBlockHandler.....................................start uri: {}, error : {}", uri, e.getMessage());
+        log.info(".....................................MyBlockHandler.....................................start uri: {}, error : {}", uri, e.getRule());
+
+        RestObject restObject;
+
+        // 不同的异常返回不同的提示语
+        if (e instanceof FlowException) {
+            restObject = RestObject.builder().statusCode(HttpStatus.TOO_MANY_REQUESTS.value()).statusMessage("接口限流了").build();
+
+        } else if (e instanceof DegradeException) {
+            restObject = RestObject.builder().statusCode(HttpStatus.SERVICE_UNAVAILABLE.value()).statusMessage("服务降级了").build();
+
+        } else if (e instanceof ParamFlowException) {
+            restObject = RestObject.builder().statusCode(HttpStatus.PRECONDITION_FAILED.value()).statusMessage("热点参数限流了").build();
+
+        } else if (e instanceof SystemBlockException) {
+            restObject = RestObject.builder().statusCode(HttpStatus.NOT_ACCEPTABLE.value()).statusMessage("触发系统保护规则").build();
+
+        } else if (e instanceof AuthorityException) {
+            restObject = RestObject.builder().statusCode(HttpStatus.UNAUTHORIZED.value()).statusMessage("授权规则不通过").build();
+
+        } else {
+            restObject = RestObject.builder().statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value()).statusMessage("系统繁忙").build();
+        }
 
         //返回json数据
-        response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+        response.setStatus(restObject.getStatusCode());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setCharacterEncoding("utf-8");
         PrintWriter out = response.getWriter();
-        out.print("{\"code\": 429, \"msg\": \"请求过于频繁，请稍后再试\"}");
+        out.print(JSON.toJSONString(restObject));
         out.flush();
         out.close();
 
